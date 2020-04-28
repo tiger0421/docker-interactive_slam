78adfb0155e880c22191aac6005cfb36c9d6811b
tiger0421 <taiger0421@yahoo.co.jp>
2020-04-29 00:59:47 +0900

    change details

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
+curl https://raw.githubusercontent.com/tiger0421/docker-interactive_slam/master/install_interactive_slam.sh > /tmp/install.sh
+chmod 774 install.sh
 source /opt/ros/melodic/setup.bash
-
-cd /root
-# create workspace
-mkdir -p ~/catkin_ws/src
-cd /root/catkin_ws/src
-catkin_init_workspace
-cd /root/catkin_ws
-catkin build
-
-# clone interactive_slam
-cd /root/catkin_ws/src
-git clone https://github.com/koide3/ndt_omp
-git clone https://github.com/koide3/hdl_graph_slam
-git clone https://github.com/koide3/odometry_saver
-git clone https://github.com/SMRT-AIST/interactive_slam --recursive
-cd /root/catkin_ws
-catkin build -DCMAKE_BUILD_TYPE=Release
+sh /tmp/install.sh
+rm /tmp/install.sh
+echo '#!/bin/sh' > /tmp/install.sh
 
 exec "$@"
