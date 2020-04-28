dfef6b54099f77196539bd679a69a33b70ac7322
tiger0421 <taiger0421@yahoo.co.jp>
2020-04-29 00:41:16 +0900

    fix DIR

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-ource /opt/ros/melodic/setup.bash
+source /opt/ros/melodic/setup.bash
 
+cd /root
 # create workspace
 mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
+cd /root/catkin_ws/src
 catkin_init_workspace
-cd ~/catkin_ws
+cd /root/catkin_ws
 catkin build
 
 # clone interactive_slam
-cd ~/catkin_ws/src
+cd /root/catkin_ws/src
 git clone https://github.com/koide3/ndt_omp
 git clone https://github.com/koide3/hdl_graph_slam
 git clone https://github.com/koide3/odometry_saver
 git clone https://github.com/SMRT-AIST/interactive_slam --recursive
-cd ~/catkin_ws
+cd /root/catkin_ws
 catkin build -DCMAKE_BUILD_TYPE=Release
 
 exec "$@"
